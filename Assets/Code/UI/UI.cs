e0159a3efdb387233c3e1af0db54c73b795236d0
Mário Fernandes <ask.mariofernandes@gmail.com>
2020-04-29 16:53:22 +0100

    UI on Main scene and crosshair

---
+++ Assets/Code/UI/UI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class UI : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Menu;
+    public GameObject ButtonStart;
+    public GameObject ButtonResume;
+    public GameObject InGame;
+
+    private void Start()
+    {
+        Player.GetComponent<FirstPersonController>().enabled = false;
+        InGame.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            InGame.SetActive(false);
+            Menu.SetActive(true);
+            Player.GetComponent<FirstPersonController>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+    }
+
+    public void StartGame()
+    {
+        ButtonStart.SetActive(false);
+        ButtonResume.SetActive(true);
+
+        Menu.SetActive(false);
+        InGame.SetActive(true);
+        Player.GetComponent<FirstPersonController>().enabled = true;
+    }
+
+    public void ResumeGame()
+    {
+        Menu.SetActive(false);
+        InGame.SetActive(true);
+        Player.GetComponent<FirstPersonController>().enabled = true;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
