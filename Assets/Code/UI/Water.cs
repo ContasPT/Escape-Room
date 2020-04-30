9d8cbe26e7cd5f57e1f8c995998b2ca74aea2382
Mário Fernandes <ask.mariofernandes@gmail.com>
2020-04-30 16:26:55 +0100

    Water Part

---
+++ Assets/Code/UI/Water.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Water : MonoBehaviour
+{
+
+    public GameObject Pos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(teleport(other));
+            
+        }
+    }
+
+    IEnumerator teleport(Collider other)
+    {
+        // suspend execution for 5 seconds
+        other.GetComponent<FirstPersonController>().enabled = false;
+        other.transform.position = Pos.transform.position;
+        yield return new WaitForSeconds(0.25f);
+        while (other.transform.position != Pos.transform.position)
+        {
+            
+        }
+        other.GetComponent<FirstPersonController>().enabled = true;
+    }
+}
---
+++ Assets/Code/UI/WaterCheckPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCheckPoint : MonoBehaviour
+{
+    public GameObject Water;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            Water.GetComponent<Water>().Pos = gameObject;
+    }
+}
