af9355e456fba82274f78f91c02e318f3e94d115
Mário Fernandes <ask.mariofernandes@gmail.com>
2020-05-01 20:13:54 +0100

    Lever Puzzel

---
+++ Assets/Code/OpenDoorLever.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorLever : MonoBehaviour
+{
+
+    public Lever Lever1;
+    public Lever Lever2;
+    public Lever Lever3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Lever1.State && Lever2.State && !Lever3.State)
+            GetComponent<AN_DoorScript>().isOpened = true;
+    }
+}
