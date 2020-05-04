1f7333b63546ebe28d4584c36da64ec61ec8534f
Mário Fernandes <ask.mariofernandes@gmail.com>
2020-05-04 12:50:42 +0100

    Buttons

---
+++ Assets/Code/ButtonInter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInter : InteractiveObject
+{
+    Light light;
+
+    public float fadetime;
+    float currenttime;
+    float Normalintensity;
+
+    public ButtonPuzzel.Buttons Type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = transform.GetChild(1).GetComponent<Light>();
+        Normalintensity = light.intensity;
+        light.intensity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currenttime > 0)
+        {
+            light.intensity = currenttime * Normalintensity / fadetime;
+
+            currenttime -= Time.deltaTime;
+        }
+    }
+
+    public override void ExecuteInteractiveAction()
+    {
+        Light();
+        transform.parent.gameObject.GetComponent<ButtonPuzzel>().PressButton(Type);
+    }
+
+    public void Light()
+    {
+        currenttime = fadetime;
+    }
+}
---
+++ Assets/Code/ButtonPuzzel.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPuzzel : MonoBehaviour
+{
+
+    public List<Buttons> sequence;
+
+    [SerializeField]
+    List<Buttons> InputButtons;
+
+    bool pass = false;
+
+    bool Start = true;
+
+    public enum Buttons
+    {
+        Blue,
+        Green,
+        Red
+    }
+    private void Update()
+    {
+        if(Start && InputButtons.Count > 0)
+        {
+            InputButtons.Clear();
+            StartCoroutine(StartSequance());
+        }
+
+        if (sequence.Count == InputButtons.Count && !Start)
+        {
+            pass = true;
+
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                if (sequence[i] != InputButtons[i])
+                    pass = false;
+            }
+
+            if (pass)
+                Pass();
+            else
+                Fail();
+        }
+    }
+
+    public void PressButton(Buttons Inp_Button)
+    {
+        InputButtons.Add(Inp_Button);
+    }
+
+    void Pass()
+    {
+        InputButtons.Clear();
+        SFXManager.TheSFXGuy.PlaySFX("Success");
+    }
+
+    void Fail()
+    {
+        Start = true;
+        InputButtons.Clear();
+        SFXManager.TheSFXGuy.PlaySFX("Fail");
+    }
+
+    public IEnumerator StartSequance()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<ButtonInter>().tag = "Untagged";
+            transform.GetChild(i).GetComponent<ButtonInter>().Light();
+        }
+        yield return new WaitForSeconds(3);
+
+        for (int i = 0; i < sequence.Count; i++)
+        {
+            for (int o = 0; o < transform.childCount; o++)
+            {
+                if (transform.GetChild(o).GetComponent<ButtonInter>().Type == sequence[i])
+                {
+                    transform.GetChild(o).GetComponent<ButtonInter>().Light();
+                    break;
+                }
+                
+            }
+            yield return new WaitForSeconds(2);
+        }
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<ButtonInter>().tag = "Interactable";            
+        }
+
+        Start = false;
+    }
+}
